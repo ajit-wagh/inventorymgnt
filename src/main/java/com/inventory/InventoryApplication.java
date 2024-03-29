4b20f9cf9854af5adf3f35f37087b5bc236f1c4e
ajit-wagh <68419476+ajit-wagh@users.noreply.github.com>
2024-03-29 10:55:29 +0530

    h

--- src/main/java/com/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/InventoryApplication.java
@@ -6,19 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryApplication.class, args);
-		System.out.println("Hello World");
-		int j=0;
-		if(true){
-			jhjvb
-			n n 	grf				                                                                                                                              
-			                                                          
-			                                                                                                    
-		System.out.println("chechttps://google.github.io/styleguide/javaguide.html\r\n"
-				+ "k");
-		}
-		System.out.println("Checking Pre commit hook..........!");
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(InventoryApplication.class, args);
+    System.out.println("Hello World");
+    int j = 0;
+    if (true) {
+      System.out.println("chechttps://google.github.io/styleguide/javaguide.html\r\n");
+    }
+    System.out.println("Checking Pre commit hook..........!");
+  }
 }
